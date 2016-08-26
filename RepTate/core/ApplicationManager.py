bbfe677da9d9a5794fd16fb8b45acc364fe1b90a
Jorge <jorge.ramirez@upm.es>
2016-08-26 19:00:04 -0400

    Added some new text commands and completions

--- RepTate/core/ApplicationManager.py
+++ RepTate/core/ApplicationManager.py
@@ -1,18 +1,38 @@
+import os
 import cmd
+import sys
+import logging
+import logging.handlers
 from ApplicationTest import *
 
 class ApplicationManager(cmd.Cmd):
     """Main Reptate container of applications"""
 
+    version = '0.1'
     prompt = 'reptate> '
-    intro = 'RepTate command processor'
+    intro = 'Reptate Version %s command processor'%version
     
     def __init__ (self, parent=None):
+        """Constructor """
         cmd.Cmd.__init__(self)
+        self.setup_applications()
+        self.setup_log()
+        
+    def setup_log(self):
+        self.reptatelogger = logging.getLogger('ReptateLogger')
+        self.reptatelogger.setLevel(logging.DEBUG) # INFO, WARNING
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        #log_file_name = 'reptate.log'
+        #handler = logging.handlers.RotatingFileHandler(
+        #    log_file_name, maxBytes=20000, backupCount=2, mode='w')
+        #MainWindow.reptatelogger.addHandler(handler)
+
+    def setup_applications(self):
         self.application_counter=0
         self.applications=[]
         self.available_applications=[]
         self.available_applications.append(ApplicationTest)
+        self.current_application=None
 
     def do_list_available_applications(self, line):
         """List available applications"""
@@ -24,14 +44,39 @@ class ApplicationManager(cmd.Cmd):
         for app in self.applications:
             print("%s: %s"%(app.name,app.description))
 
+    def do_switch_application(self, line):
+        """Change to another open application"""
+        for app in self.applications:
+            if (app.name==line):
+                self.current_application=app    
+
+    def complete_switch_application(self, text, line, begidx, endidx):
+        completions = complete_delete_application(text, line, begidx, endidx)
+        return completions
+
     def do_delete_application(self, line):
         """Delete an open application"""
         for app in self.applications:
             if (app.name==line):
                 self.applications.remove(app)
 
+    def complete_delete_application(self, text, line, begidx, endidx):
+        """Complete delete application command"""
+        app_names=[]
+        for app in self.applications:
+            app_names.append(app.name)
+        if not text:
+            completions = app_names[:]
+        else:
+            completions = [ f
+                            for f in app_names
+                            if f.startswith(text)
+                            ]
+        return completions
+
     def do_EOF(self, line):
         """Exit RepTate"""
+        self.reptatelogger.debug("Exiting RepTate...")
         return True
 
     def do_new_application(self, name):
@@ -41,7 +86,27 @@ class ApplicationManager(cmd.Cmd):
             newapp=ApplicationTest()
             newapp.name=newapp.name+str(self.application_counter)
             self.applications.append(newapp)
+            self.current_application=newapp
         else:
             print("Application %s is not available"%name)            
+    
+    def complete_new_application(self, text, line, begidx, endidx):
+        """Complete command"""
+        app_names=[]
+        for app in self.available_applications:
+            app_names.append(app.name)
+        if not text:
+            completions = app_names[:]
+        else:
+            completions = [ f
+                            for f in app_names
+                            if f.startswith(text)
+                            ]
+        return completions
 
-
+    def do_shell(self, line):
+        "Run a shell command"
+        print("running shell command:", line)
+        output = os.popen(line).read()
+        print(output)
+        self.last_output = output
---
+++ RepTate/core/Filetype.py
@@ -0,0 +1,85 @@
+import numpy as np
+import logging
+#from Table import *
+
+class TXTColumnFile:
+	
+    """Basic class for text-column based data files
+    
+    Columns should be separated by espaces or tabs
+    
+    """    
+    def __init__(self, name='TXTColumn', extension='txt', 
+                 description='Generic text file with columns', 
+                 num_header_lines=0, col_names_line=-1, col_names=[],
+                 col_index=[0, 1], basic_file_parameters=[], col_units=[]):
+        self.name=name
+        self.extension=extension
+        self.description=description
+        self.num_header_lines=num_header_lines
+        self.col_names_line=col_names_line
+        self.col_names=col_names
+        self.col_index=col_index
+        self.basic_file_parameters=basic_file_parameters # Those that will show by default in the dataset
+        self.col_units=col_units
+        self.logger = logging.getLogger('ReptateLogger')
+
+    def read_file(self, filename):
+        dt=Table(filename)
+        dt.data_file_type = self
+        f = open(filename, "r")
+        line=f.readline()
+        items=line.split(';')
+        dt.file_parameters={}
+        # Get Parameters
+        for i in range(len(items)):
+            par=items[i].split('=')
+            if len(par)>1:
+                dt.file_parameters[par[0]]=par[1]
+        self.logger.debug(dt.file_parameters)
+    
+        # Get Columns
+        if (self.col_names_line>0):
+            self.logger.debug(self.col_names_line)
+            for i in range(2,self.col_names_line):
+                dt.header_lines.append(f.readline())
+            line=f.readline()
+            items=line.split()
+            self.col_index=[]
+            for col in self.col_names:
+                for j in range(len(items)):
+                    if (col==items[j]):
+                        self.col_index.append(int(j))
+                        break
+        else:
+            for i in range(self.num_header_lines):
+               dt.header_lines.append(f.readline())
+        self.logger.debug(dt.header_lines)
+        self.logger.debug(self.col_index)
+        dt.num_columns=len(self.col_index)
+
+        rawdata=[]
+        while(True):
+            line=f.readline()
+            if not line: break 
+            items=line.split()
+            for j in self.col_index:
+                rawdata.append(float(items[j]))
+        dt.num_lines=int(len(rawdata)/dt.num_columns)
+        dt.data=np.reshape(rawdata,newshape=(dt.num_lines, dt.num_columns))
+        self.logger.debug(dt.data)
+        return dt
+
+if __name__ == '__main__':
+    t = TXTColumnFile()
+    # Two ways of defining the contents of the file
+    # Form 1
+    t.col_names_line=-1
+    t.num_header_lines=3
+    t.col_index=[0, 1]
+    t.col_names=['t','Gt']
+    # Form two
+    #t.col_names_line=4
+    #t.col_names=['t','Gt']
+    t.read_file("/Users/Jorge/Downloads/C0024_NVT_450K_1atm.gt")
+    
