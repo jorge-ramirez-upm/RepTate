942267c42d66d9364a69d31a94e89231774d3262
Jorge <jorge.ramirez@upm.es>
2016-08-27 22:45:50 -0400

    Small changes

--- RepTate/core/File.py
+++ RepTate/core/File.py
@@ -7,6 +7,6 @@ class File(object):
         self.file_name = file_name
         tmpname = file_name.split('/')
         self.file_name_short=tmpname[len(tmpname)-1]
-        self.file_type = None # Not sure if this is necessary
+        self.file_type = TXTColumnFile #None # Not sure if this is necessary
 
 
