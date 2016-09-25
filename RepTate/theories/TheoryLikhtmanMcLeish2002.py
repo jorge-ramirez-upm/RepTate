65f7c493c4dab8463741cf1e20a7d7949f16e710
Jorge <jorge.ramirez@upm.es>
2016-09-25 17:20:33 +0200

    Likhtman-McLeish works without interpolating Z

--- RepTate/theories/TheoryLikhtmanMcLeish2002.py
+++ RepTate/theories/TheoryLikhtmanMcLeish2002.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy import interp
 from Theory import *
 
 class TheoryLikhtmanMcLeish2002(Theory, CmdBase):
@@ -14,7 +15,7 @@ Macromolecules 2002, 35, 6332-6343"
         self.function = self.LikhtmanMcLeish2002
         self.parameters["taue"]=Parameter("taue", 2e-6, "Rouse time of one Entanglement", ParameterType.real, True)
         self.parameters["Ge"]=Parameter("Ge", 1e6, "Entanglement moduluas", ParameterType.real, True)
-        self.parameters["Me"]=Parameter("Me", 5, "Entanglement molecular weight", ParameterType.real, False)
+        self.parameters["Me"]=Parameter("Me", 5, "Entanglement molecular weight", ParameterType.real, True)
         self.parameters["cnu"]=Parameter("cnu", 0.1, "Constraint Release parameter", ParameterType.real, False)
 
         f=np.load("theories\linlin.npz")
@@ -28,13 +29,14 @@ Macromolecules 2002, 35, 6332-6343"
         tt.num_columns=ft.num_columns
         tt.num_rows=ft.num_rows
         tt.data=np.zeros((tt.num_rows, tt.num_columns))
-        #tt.data[:,0]=ft.data[:,0] # TODO!!!
+        tt.data[:,0]=ft.data[:,0] 
 
         taue=self.parameters["taue"].value
         Ge=self.parameters["Ge"].value
         Me=self.parameters["Me"].value
         cnu=self.parameters["cnu"].value
         Mw=float(f.file_parameters["Mw"])
+        
         Z=np.round(Mw/Me) # For the time being, we don't interpolate
         
         indZ = (np.where(self.Zarray==Z))[0][0]
@@ -44,8 +46,6 @@ Macromolecules 2002, 35, 6332-6343"
         ind1=1+indcnu*2
         ind2=ind1+1
 
-        for i in range(len(table)):
-            tt.data[i,0]=table[i][0]/taue
-            tt.data[i,1]=Ge*table[i][ind1]
-            tt.data[i,2]=Ge*table[i][ind2]
-               
+        tt.data[:,1]=interp(tt.data[:,0], table[:,0]/taue, Ge*table[:,ind1])
+        tt.data[:,2]=interp(tt.data[:,0], table[:,0]/taue, Ge*table[:,ind2])
+                       
