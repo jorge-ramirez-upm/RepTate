79d28a1fe89f70fccbc0d8b7b1582fa47ab7b406
Victor Boudara <mmvahb@leeds.ac.uk>
2017-11-01 23:43:37 +0000

    using np.digitize and appropriate normalization

--- RepTate/theories/TheoryDiscrMWD.py
+++ RepTate/theories/TheoryDiscrMWD.py
@@ -12,8 +12,8 @@ class TheoryDiscrMWD(Theory, CmdBase):
         super(TheoryDiscrMWD, self).__init__(name, parent_dataset, ax)
         self.function = self.DiscretiseMWD
         self.has_modes = False
-        self.parameters["binpd"] = Parameter(
-            "binpd", 10, "Number modes per decade", ParameterType.integer, False)
+        self.parameters["bpd"] = Parameter(
+            "bpd", 10, "Number modes per decade", ParameterType.integer, False)
         self.parameters["Mn"] = Parameter(
             "Mn", 1000, "Number-average molecular mass", ParameterType.real, False)
         self.parameters["Mw"] = Parameter(
@@ -23,7 +23,11 @@ class TheoryDiscrMWD(Theory, CmdBase):
         self.parameters["PDI"] = Parameter(
             "PDI", 1, "Polydispersity index", ParameterType.real, False)
 
+    def do_error(self, line):
+        pass
+
     def calculate_moments(self, f, line=""):
+        '''Calculate the moments Mn, Mw, and Mz of a molecular mass distribution'''
         n = f[:, 0].size
         Mw = 0
         tempMz = 0
@@ -46,43 +50,65 @@ class TheoryDiscrMWD(Theory, CmdBase):
             %(line, Mn/1000, Mw/1000, PDI, Mz/Mw)
             )
 
-    def do_error(self, line):
-        pass
 
     def DiscretiseMWD(self, f=None):
-        """
-        Discretize a molecular weight distribution
-        """
-        # sort M, w(M) with M increasing intoft
-        ft = f.data_table.data[np.argsort(f.data_table.data[:, 0])]
-        #normalize the weights w(M)
-        ft[:, 1] = ft[:, 1]/np.sum(ft[:, 1])
-        self.calculate_moments(ft, "input")
-        
-        #molar mass min and max
-        mmin = np.min(ft[:, 0])
-        mmax = np.max(ft[:, 0])
+        """Discretize a molecular weight distribution"""
 
-        #create theory table (tt)
-        nM = int(np.ceil((np.log10(mmax) - np.log10(mmin))*self.parameters["binpd"].value))
+        # sort M, w(M) with M increasing in ft
+        ft = f.data_table.data[np.argsort(f.data_table.data[:,0])]
+        n = ft[:, 0].size
+        #normalize the weights w(M)
+        ft[:,1] = ft[:,1]/np.sum(ft[:,1])
+        self.calculate_moments(ft, "input")
+
+        #molar mass min and max 
+        mmin = 0.5*np.min(ft[:, 0])
+        mmax = 2*np.max(ft[:, 0])
+
+        #create theory table (tt) and temp table
+        nbin = int(np.ceil((np.log10(mmax/mmin))*self.parameters["bpd"].value))
         tt = self.tables[f.file_name_short]
         tt.num_columns = 2
-        tt.num_rows = nM
+        tt.num_rows = nbin
         tt.data = np.zeros((tt.num_rows, tt.num_columns))
 
-        #(nM+1) bins equally distributed on logarithmic scale
-        bins = np.zeros(nM + 1)
-        for i in range(nM + 1):
-            bins[i] = mmin*np.power(10, i/nM * np.log10(mmax/mmin))
+        #bins equally spaced on logarithmic scale
+        bins = np.zeros(nbin)
+        for k in range(nbin):  
+            bins[k] = mmin*np.power(10, k/nbin * np.log10(mmax/mmin))
 
+        #create the edges of the bins
+        edge_bins = np.zeros(nbin - 1)
+        for k in range(nbin - 1):
+            edge_bins[k] = (np.log10(bins[k + 1]) + np.log10(bins[k])) / 2
+        
+        #create mid data point values
+        mid_ft = np.zeros(n - 1)
+        for j in range(n - 1):
+            mid_ft[j] = (np.log10(ft[j + 1, 0]) + np.log10(ft[j, 0]))/2 #mid-values of logM
 
+        #find into which bin the data point belong
+        inds = np.digitize(mid_ft, edge_bins, right=False) 
+        '''returns indices of the bins to which each value in input array belongs
+        edge_bins[inds[j]-1] <= mid_ft[j] < edge_bins[inds[j]]'''
 
-        tt.data[:, 1], bins = np.histogram(
-            ft[:, 0], bins=bins, range=(mmin, mmax), weights=ft[:, 1]
-            )
+        phi = np.zeros(nbin)
+        nk = np.zeros(nbin)
+        for j in range(n - 1):
+            k = inds[j]
+            dlogMj = np.log10(ft[j + 1, 0]) - np.log10(ft[j, 0])
+            wj = (ft[j + 1, 1] + ft[j, 1]) / 2
+            dlogMk = (np.log10(bins[k + 1])  - np.log10(bins[k - 1])) / 2
+            phi[k] +=  wj*dlogMj/dlogMk #weight x width / bin_width
+            nk[k] += 1
 
-        #Centered M
-        for i in range(nM):
-            tt.data[i, 0] = np.power(10, (np.log10(bins[i]) + np.log10(bins[i + 1]))/2)
+        phi = np.choose(nk==0, (phi/nk * np.sum(nk)/nbin, phi))
+        # for k in range (nbin):
+        #     if nk[k]>0: phi[k] = phi[k]/nk[k] * np.sum(nk)/nbin
+        #     else: phi[k] = phi[k]
+
+        #copy weights and M into theory table
+        tt.data[:, 0] = bins
+        tt.data[:, 1] = phi/np.sum(phi)
 
         self.calculate_moments(tt.data, "discretized")
