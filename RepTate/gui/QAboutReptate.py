e39c6a410b84bc04a717998d30703020db9f90ee
Jorge <jorge.ramirez@upm.es>
2017-11-25 16:10:49 +0100

    Added tests files Update comment in QAboutReptate.py to see if it shows up in the documentation

--- RepTate/gui/QAboutReptate.py
+++ RepTate/gui/QAboutReptate.py
@@ -6,8 +6,9 @@ path = os.path.dirname(os.path.abspath(__file__))
 Ui_AboutReptateWindow, QDialog = loadUiType(os.path.join(path,'AboutDialog.ui'))
 
 class AboutWindow(QDialog, Ui_AboutReptateWindow):
-        def __init__(self, parent, version):
-            super(AboutWindow, self).__init__(parent)
-            self.setupUi(self)
-            self.label_Version.setText('RepTate v'+version)
+    """About window in the GUI"""    
+    def __init__(self, parent, version):
+        super(AboutWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.label_Version.setText('RepTate v'+version)
     
