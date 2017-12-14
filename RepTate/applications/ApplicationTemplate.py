825c8c7b8e2bdc5aca1d7f63300c8f702d4659d8
Victor Boudara <victor.boudara@gmail.com>
2017-12-14 15:05:34 +0000

    added application template file

---
+++ RepTate/applications/ApplicationTemplate.py
@@ -0,0 +1,132 @@
+# RepTate: Rheology of Entangled Polymers: Toolkit for the Analysis of Theory and Experiments
+# http://blogs.upm.es/compsoftmatter/software/reptate/
+# https://github.com/jorge-ramirez-upm/RepTate
+# http://reptate.readthedocs.io
+# Jorge Ramirez, jorge.ramirez@upm.es
+# Victor Boudara, mmvahb@leeds.ac.uk
+# Copyright (2017) Universidad Politécnica de Madrid, University of Leeds
+# This software is distributed under the GNU General Public License. 
+"""Module ApplicationTemplate
+
+Template file for the definition of a new Application Module
+
+""" 
+from CmdBase import CmdBase, CmdMode
+from Application import Application
+from QApplicationWindow import QApplicationWindow
+from View import View
+from FileType import TXTColumnFile
+import numpy as np
+
+# IMPORT THEORIES
+# Import theories specific to the Application e.g.:
+# from TheoryTemplate import TheoryA
+
+
+class ApplicationTemplate(CmdBase):
+    """[summary]
+    
+    [description]
+    """
+    name = 'Template'
+    description = 'Template Application' #used in the command-line Reptate
+
+    def __new__(cls, name='Template', parent=None):
+        """[summary]
+        
+        [description]
+        
+        Keyword Arguments:
+            name {[type]} -- [description] (default: {'Template'})
+            parent {[type]} -- [description] (default: {None})
+        
+        Returns:
+            [type] -- [description]
+        """
+        return GUIApplicationTemplate(name, parent) if (CmdBase.mode == CmdMode.GUI) else CLApplicationTemplate(name, parent)
+
+class BaseApplicationTemplate:
+    """[summary]
+    
+    [description]
+    """
+    def __init__(self, name='Template', parent=None):
+        """[summary]
+        
+        [description]
+        
+        Keyword Arguments:
+            name {[type]} -- [description] (default: {'Template'})
+            parent {[type]} -- [description] (default: {None})
+        """
+        super().__init__(name, parent)
+
+        # VIEWS
+        # set the views that can be selected in the view combobox
+        # the order is defined by the index, index 0 is the defaut view
+        self.views['y(x)'] = View(name='y(x)', description='y as a function of x', x_label='x', y_label='y(x)', x_units='-', y_units='-', log_x=False, log_y=False, view_proc=self.viewyx, n=1, snames=['y(x)'], index=0)
+
+        # FILES
+        # set the type of files that ApplicationTemplate can open
+        ftype = TXTColumnFile(name='content of files', extension='txt', description='description of the file type', col_names=['col1','col2'], basic_file_parameters=['param1','param2'], col_units=['units_col1','units_col2'])
+        self.filetypes[ftype.extension] = ftype #add each the file type to dictionary
+
+        # THEORIES
+        # add the theories related to ApplicationTemplate to the dictionary, e.g.:
+        # self.theories[TheoryA.thname] = TheoryA
+
+    def viewyx(self, dt, file_parameters):
+        """[summary]
+        
+        [description]
+        
+        Arguments:
+            dt {[type]} -- [description]
+            file_parameters {[type]} -- [description]
+        
+        Returns:
+            [type] -- [description]
+        """
+        x = np.zeros((dt.num_rows, 1))
+        y = np.zeros((dt.num_rows, 1))
+        x[:, 0] = dt.data[:, 0]
+        y[:, 0] = dt.data[:, 1]
+        return x, y, True
+
+
+class CLApplicationTemplate(BaseApplicationTemplate, Application):
+    """[summary]
+    
+    [description]
+    """
+    def __init__(self, name='Template', parent=None):
+        """[summary]
+        
+        [description]
+        
+        Keyword Arguments:
+            name {[type]} -- [description] (default: {'Template'})
+            parent {[type]} -- [description] (default: {None})
+        """
+        super().__init__(name, parent)
+        #usually this class stays empty
+
+class GUIApplicationTemplate(BaseApplicationTemplate, QApplicationWindow):
+    """[summary]
+    
+    [description]
+    """
+    def __init__(self, name='Template', parent=None):
+        """[summary]
+        
+        [description]
+        
+        Keyword Arguments:
+            name {[type]} -- [description] (default: {'Template'})
+            parent {[type]} -- [description] (default: {None})
+        """
+        super().__init__(name, parent)
+
+        #add the GUI-specific objects here:
+        self.populate_views() #populate the view ComboBox
+        
