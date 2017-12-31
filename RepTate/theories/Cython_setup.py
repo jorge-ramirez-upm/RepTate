4e0b3c5b2eb967ea52acbb7121b831cc17250b7f
Victor B <victor.boudara@gmail.com>
2017-12-31 16:56:11 +0100

    Delete Cython_setup.py

--- RepTate/theories/Cython_setup.py
+++
@@ -1,8 +0,0 @@
-#Setup file for Cython: pre-compile functions
-#py Cython_setup.py build_ext --inplace
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(
-	ext_modules = cythonize(["scilength.pyx", "brlength.pyx", "request_arm.pyx"])
-)
