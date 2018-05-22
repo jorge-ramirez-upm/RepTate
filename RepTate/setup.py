5ebc3c120d5718d20102a79c34c93035f6e4862d
Jorge Ramirez <jorge.ramirez@upm.es>
2018-05-22 14:47:44 +0200

    Updated setup.py (for conda package?)

--- RepTate/setup.py
+++ RepTate/setup.py
@@ -1,20 +1,41 @@
+import os
+from setuptools import setup, find_packages
 import sys
-from distutils.core import setup
-import py2exe
 
 sys.path.append('core')
 sys.path.append('gui')
 sys.path.append('console')
 sys.path.append('applications')
 sys.path.append('theories')
-sys.path.append('visual')
+sys.path.append('tools')
 
+import Version
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+requirements_file = open(os.path.join(base_dir, 'requirements.txt'))
+requirements = requirements_file.read().splitlines()
 
 setup(
     name="RepTate",
-    version="0.5",
+	version=Version.VERSION,
     description="Rheology of Entangled Polymers: Toolkit for the Analysis of Theory and Experiment",
-    author="Jorge Ramirez",
+    author="Jorge Ramirez <jorge.ramirez@upm.es>, Victor Boudara <V.A.Boudara@leeds.ac.uk>",
     author_email="jorge.ramirez@upm.es",
-    console=[{'script': 'RepTate.py'}],
+	url='http://github.com/jorge-ramirez-upm/RepTate',
+	packages=find_packages(),
+	install_requires=requirements,
+	entry_points={
+        'console_scripts': [
+            'RepTate = RepTate:__main__',
+            'RepTateCL = RepTateCL:__main__'
+        ]
+    },
+	license='GPL License',
+	classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+		'Topic :: Scientific/Engineering',
+		'Intended Audience :: Science/Research'
+    ]
 )
