496cde5649df89a1cc35510828f51982164c76f5
Victor Boudara <victor.boudara@gmail.com>
2018-11-27 14:14:53 +0000

    adjust extra LML widgets

--- RepTate/theories/TheoryLikhtmanMcLeish2002.py
+++ RepTate/theories/TheoryLikhtmanMcLeish2002.py
@@ -43,8 +43,8 @@ from CmdBase import CmdBase, CmdMode
 from Theory import Theory
 from QTheory import QTheory
 from Parameter import Parameter, ParameterType, OptType
-from PyQt5.QtWidgets import QToolBar, QAction, QStyle, QLabel, QLineEdit
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QToolBar, QAction, QStyle, QLabel, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon, QDoubleValidator
 from PyQt5.QtCore import QSize
 
 class TheoryLikhtmanMcLeish2002(CmdBase):
@@ -293,21 +293,47 @@ class GUITheoryLikhtmanMcLeish2002(BaseTheoryLikhtmanMcLeish2002, QTheory):
         tb.addWidget(lbl)
         self.txtrho = QLineEdit("%.4g"%self.parameters["rho0"].value)
         self.txtrho.setReadOnly(True)
+        self.txtrho.setDisabled(True)
+        dvalidator = QDoubleValidator()  #prevent letters etc.
+        dvalidator.setBottom(0)  #minimum allowed value
+        dvalidator.setTop(10)  #maximum allowed value
+        self.txtrho.setValidator(dvalidator)
         tb.addWidget(self.txtrho)
         self.thToolsLayout.insertWidget(0, tb)
         
         connection_id = self.linkMeGeaction.triggered.connect(self.linkMeGeaction_change)
+        connection_id = self.txtrho.textEdited.connect(self.handle_txtrho_edited)
 
-    def linkMeGeaction_change(self):
-        self.set_param_value('linkMeGe', self.linkMeGeaction.isChecked())
-        if self.linkMeGeaction.isChecked():
+    def linkMeGeaction_change(self, checked):
+        self.set_param_value('linkMeGe', checked)
+        if checked:
             self.txtrho.setReadOnly(False)
+            self.txtrho.setDisabled(False)
             p = self.parameters['Ge']
             p.opt_type=OptType.const
         else:
             self.txtrho.setReadOnly(True)
+            self.txtrho.setDisabled(True)
             p = self.parameters['Ge']
             p.opt_type=OptType.opt
         self.update_parameter_table()
+        if self.autocalculate:
+            self.handle_actionCalculate_Theory()
 
-            
+    def handle_txtrho_edited(self, new_text):
+        try:
+            val = float(new_text)
+        except ValueError:
+            QMessageBox.warning(self, 'Error', 'Could not convert "%s" to float' % new_text)
+            self.txtrho.setText("%.4g" % self.parameters["rho0"].value)
+        else:
+            self.set_param_value('rho0', val)
+            if self.autocalculate:
+                self.handle_actionCalculate_Theory()
+
+    def set_extra_data(self, _):
+        """Restaure the check state of button and text value"""
+        self.txtrho.setText("%.4g" % self.parameters["rho0"].value)
+        checked = self.parameters['linkMeGe'].value
+        self.linkMeGeaction.setChecked(checked)
+        self.linkMeGeaction_change(checked)
