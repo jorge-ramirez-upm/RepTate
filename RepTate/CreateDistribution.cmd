8b791d2e9e463c25dc010f4a970d23021c5994d9
Jorge Ramirez <jorge.ramirez@upm.es>
2019-12-28 13:53:26 +0100

    Update CreateDistribution.cmd to work with Winpython

--- RepTate/CreateDistribution.cmd
+++ RepTate/CreateDistribution.cmd
@@ -1,7 +1,10 @@
 echo off
 rem CREATE GUI Version First
+rem INSTALL PACKAGING "pip install packaging"
 rmdir /s /q build\RepTate
 rmdir /s /q dist\RepTate
+rem ADAPT THE FOLLOWING LINE TO MATCH YOUR PYTHON DISTRIBUTION
+set PATH=%PATH%;c:\WPy64-3741\python-3.7.4.amd64\Lib\site-packages\scipy\.libs\;
 pyinstaller -w -i gui\Images\Reptate64.ico --hidden-import=packaging --hidden-import=packaging.version --hidden-import=packaging.specifiers --hidden-import=packaging.requirements --hidden-import=scipy._lib.messagestream --hidden-import=pandas._libs.tslibs.timedeltas -p applications;core;theories;tools;gui RepTate.py
 mkdir dist\RepTate\gui
 copy gui\theorytab.ui dist\RepTate
@@ -22,7 +25,7 @@ copy ..\Reptate_license.txt dist\RepTate
 copy tools\materials_database.npy dist\RepTate
 copy tools\user_database.npy dist\RepTate
 copy tools\polymer_data.py dist\RepTate
-copy c:\Miniconda3\Library\bin\mkl_def.dll dist\RepTate
+rem copy c:\Miniconda3\Library\bin\mkl_def.dll dist\RepTate
 mkdir dist\RepTate\data
 xcopy data dist\RepTate\data /E
 mkdir dist\RepTate\theories
