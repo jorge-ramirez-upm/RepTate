ab8643af89192f052de6dc386936404b6040b688
Jorge Ramirez <jorge.ramirez@upm.es>
2020-04-27 11:12:23 +0200

    NEW Theory DSM (Slip links, Jay Schieber)

--- RepTate/theories/TheoryDSMLinear.py
+++ RepTate/theories/TheoryDSMLinear.py
@@ -46,7 +46,7 @@ class TheoryDSMLinear(CmdBase):
 
     thname = "CFSM+Rouse"
     description = "Clustered Fixed Slip Link theory for linear entangled polymers"
-    citations = "Katzarova, M., Yang, L., Andreev, M., Córdoba, A., & Schieber, J. D., Rheologica Acta 2015, 54(3), 169-183."
+    citations = "Katzarova, M. et al, Rheol Acta 2015, 54(3), 169-183."
     doi = "https://doi.org/10.1007/s00397-015-0836-0"
 
     def __new__(cls, name='', parent_dataset=None, axarr=None):
@@ -70,6 +70,65 @@ class BaseTheoryDSMLinear:
         super().__init__(name, parent_dataset, axarr)
         self.function = self.calculate  # main theory function
         self.has_modes = False  # True if the theory has modes
+
+        # Declare theory parameters and Material parameters
+        # self.parameters["Mc"] = Parameter(
+        #     name=r'M_c',
+        #     value=Mc,
+        #     description="Molecular weight of a cluster in Da",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        # self.parameters["Nc"] = Parameter(
+        #     name=r'N_c',
+        #     value=Nc,
+        #     description="Number of entanglement clusters",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        # self.parameters["tau_c"] = Parameter(
+        #     name=r'tau_c',
+        #     value=tau_c,
+        #     description="Time constant used to fit CFSM results (beta = 1) to experimental data",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        # self.parameters["beta"] = Parameter(
+        #     name=r'beta',
+        #     value=beta,
+        #     description="Entanglement activity parameter for input to DSM simulations",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        self.parameters["MK"] = Parameter(
+            name=r'MK',
+            value=300,
+            description="Molecular weight of Kuhn step (Da)",
+            type=ParameterType.real,
+            opt_type=OptType.const,
+            display_flag=True)
+
+        self.parameters["rho0"] = Parameter(
+            name=r'rho0',
+            value=1.0,
+            description="Density (g/cc)",
+            type=ParameterType.real,
+            opt_type=OptType.const,
+            display_flag=True)
+
+        # self.parameters["tau_K"] = Parameter(
+        #     name=r'tau_K',
+        #     value=tau_K,
+        #     description="Time constant for comparing DSM results to experimental data",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        self.get_material_parameters()
 
     def tandelta(self, omega, data):
         """Calculate the interpolated tan(delta)"""
@@ -116,7 +175,7 @@ class BaseTheoryDSMLinear:
 
         return omega_range
 
-    def set_linear_params(self,Nc):
+    def set_linear_params(self):
         """Returns fixed parameters for calculating linear chain G* data"""
 
         alpha1 = [-0.00051, -0.0205]
@@ -127,10 +186,10 @@ class BaseTheoryDSMLinear:
         tau3 = [3.110954, 0.022615]
         tau4 = [3.4840295, 0.0142809]
 
-        alpha = [alpha1[0]*Nc + alpha1[1], alpha2[0]*Nc + alpha2[1], \
-                 alpha3[0]/Nc + alpha3[1] + alpha3[2]*Nc]
-        tau = [tau1[1]*Nc**tau1[0], tau2[1]*Nc**tau2[0], \
-               tau3[1]*Nc**tau3[0], tau4[1]*Nc**tau4[0]]
+        alpha = [alpha1[0]*self.Nc + alpha1[1], alpha2[0]*self.Nc + alpha2[1], \
+                 alpha3[0]/self.Nc + alpha3[1] + alpha3[2]*self.Nc]
+        tau = [tau1[1]*self.Nc**tau1[0], tau2[1]*self.Nc**tau2[0], \
+               tau3[1]*self.Nc**tau3[0], tau4[1]*self.Nc**tau4[0]]
 
         alphaR = [0.64635, -0.4959, -1.2716]
         tauR = [6.313268381616272*(10**-9), 2.181509372282138*(10**-7), 0.797317365925168, 18.201382525250114]
@@ -178,56 +237,67 @@ class BaseTheoryDSMLinear:
         return G0*omega**2*sumGp1/sumGp2 + 1j*(G0*omega*sumGdp1/sumGdp2) 
 
 
-    def set_DSM_params(self,Mc,Nc,tau_c,beta,N_K,tau_K):
+    def do_error(self, line):
+        super().do_error(line)
+        self.print_DSM_params()
+
+    def print_DSM_params(self):
         """Print out parameters for DSM simulations"""
 
-        self.parameters["Mc"] = Parameter(
-            name=r'M_c',
-            value=Mc,
-            description="Molecular weight of a cluster in Da",
-            type=ParameterType.real,
-            opt_type=OptType.const,
-            display_flag=True)
+        self.Qprint("<b>Parameters of DSM simulations:</b>")
+        tab_data = [['%-18s' % 'Name', '%-18s' % 'Value']]
+        tab_data.append(['<b>%-18s</b>'%'Mc', '%18.4g' % self.Mc])
+        tab_data.append(['%-18s'%'<b>Nc</b>', '%18.4g' % self.Nc])
+        tab_data.append(['%-18s'%'<b>tau_c</b>', '%18.4g' % self.tau_c])
+        tab_data.append(['%-18s'%'<b>beta</b>', '%18.4g' % self.beta])
+        tab_data.append(['%-18s'%'<b>N_K</b>', '%18.4g' % self.N_K])
+        tab_data.append(['%-18s'%'<b>tau_K</b>', '%18.4g' % self.tau_K])
+        self.Qprint(tab_data)
 
-        self.parameters["Nc"] = Parameter(
-            name=r'N_c',
-            value=Nc,
-            description="Number of entanglement clusters",
-            type=ParameterType.real,
-            opt_type=OptType.const,
-            display_flag=True)
 
-        self.parameters["tau_c"] = Parameter(
-            name=r'tau_c',
-            value=tau_c,
-            description="Time constant used to fit CFSM results (beta = 1) to experimental data",
-            type=ParameterType.real,
-            opt_type=OptType.const,
-            display_flag=True)
+        # #PRINT DSM PARAMETERS
+        # self.print_DSM_params(Mc,Nc,tau_c,beta,N_K,tau_K)
 
-        self.parameters["beta"] = Parameter(
-            name=r'beta',
-            value=beta,
-            description="Entanglement activity parameter for input to DSM simulations",
-            type=ParameterType.real,
-            opt_type=OptType.const,
-            display_flag=True)
+        # self.parameters["Mc"] = Parameter(
+        #     name=r'M_c',
+        #     value=Mc,
+        #     description="Molecular weight of a cluster in Da",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
 
-        self.parameters["N_K"] = Parameter(
-            name=r'N_K',
-            value=N_K,
-            description="Number of Kuhn steps for input into DSM simulation",
-            type=ParameterType.real,
-            opt_type=OptType.const,
-            display_flag=True)
+        # self.parameters["Nc"] = Parameter(
+        #     name=r'N_c',
+        #     value=Nc,
+        #     description="Number of entanglement clusters",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
 
-        self.parameters["tau_K"] = Parameter(
-            name=r'tau_K',
-            value=tau_K,
-            description="Time constant for comparing DSM results to experimental data",
-            type=ParameterType.real,
-            opt_type=OptType.const,
-            display_flag=True)
+        # self.parameters["tau_c"] = Parameter(
+        #     name=r'tau_c',
+        #     value=tau_c,
+        #     description="Time constant used to fit CFSM results (beta = 1) to experimental data",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        # self.parameters["beta"] = Parameter(
+        #     name=r'beta',
+        #     value=beta,
+        #     description="Entanglement activity parameter for input to DSM simulations",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
+        # self.parameters["tau_K"] = Parameter(
+        #     name=r'tau_K',
+        #     value=tau_K,
+        #     description="Time constant for comparing DSM results to experimental data",
+        #     type=ParameterType.real,
+        #     opt_type=OptType.const,
+        #     display_flag=True)
+
 
     def calculate(self, f=None):
         """ 
@@ -251,8 +321,8 @@ class BaseTheoryDSMLinear:
         tt.data = np.zeros((tt.num_rows, tt.num_columns))
         tt.data[:, 0] = ft.data[:, 0]
 
-        MK = float(f.file_parameters["MK"]) #units of Da
-        rho0 = float(f.file_parameters["rho0"]) #units of g/cc
+        MK = self.parameters["MK"].value
+        rho0 = self.parameters["rho0"].value
         Mw = float(f.file_parameters["Mw"])*1000.0 #units of Da
         T = float(f.file_parameters["T"]) + 273.15 #units of K
         R = 8.314462*10**3 #units of L Pa K^-1 mol^-1
@@ -266,28 +336,25 @@ class BaseTheoryDSMLinear:
         [omega_x, Gx] = self.Gslfx(crossover_limits,data=ft.data)
         solNc = optimize.brentq(self.solveNc,a=1,b=1000,args=(Gx,Mw,rho0,R,T))
         if solNc>0:
-            Nc = solNc
-            Mc = Mw/Nc
-        tau_c = 151.148/(omega_x*Nc**3.50)
-        beta = Mw/(0.56*Nc*MK) - 1
-        tau_K = tau_c/(0.265*beta**(8.0/3.0))
-        N_K = Mw/MK
-
-        #PRINT DSM PARAMETERS
-        self.set_DSM_params(Mc,Nc,tau_c,beta,N_K,tau_K)
+            self.Nc = solNc
+            self.Mc = Mw/self.Nc
+        self.tau_c = 151.148/(omega_x*self.Nc**3.50)
+        self.beta = Mw/(0.56*self.Nc*MK) - 1
+        self.tau_K = self.tau_c/(0.265*self.beta**(8.0/3.0))
+        self.N_K = Mw/MK
 
         #- - - - - - - - - - - - - - - - - - - - - - -
         # CALCULATE CFSM AND HIGH FREQUENCY ROUSE RELAXATION MODULUS
-        params=self.set_linear_params(Nc=Nc)
+        params=self.set_linear_params()
 
         G0 = rho0*R*T/Mw
         params.append(G0)
 
         Gstar_vec = np.vectorize(self.Gstar,excluded=['Rouse','params']) 
 
-        CFSM = 0.5*(Nc-3)*Gstar_vec(omega=tt.data[:,0]*tau_c, params=params, Rouse=False)
-        prefactor = G0*((N_K+beta)/(beta+1))
-        Rouse = prefactor*Gstar_vec(omega=(tt.data[:,0]*tau_K*(beta**2)), params=params, Rouse=True)
+        CFSM = 0.5*(self.Nc-3)*Gstar_vec(omega=tt.data[:,0]*self.tau_c, params=params, Rouse=False)
+        prefactor = G0*((self.N_K+self.beta)/(self.beta+1))
+        Rouse = prefactor*Gstar_vec(omega=(tt.data[:,0]*self.tau_K*(self.beta**2)), params=params, Rouse=True)
 
         Gstar = CFSM + Rouse #G* has units of kPa
 
