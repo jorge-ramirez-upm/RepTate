2ca8dc858481fd192e9e1c480cdad795d9083500
Jorge Ramirez <jorge.ramirez@upm.es>
2020-05-13 01:33:05 +0200

    Script to traverse all py files in site-packages, byte compile them and remove original source

---
+++ scripts/traverse.py
@@ -0,0 +1,17 @@
+import os
+import py_compile
+import shutil
+
+# Set the directory you want to start from
+rootDir = '.'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    #print('Found directory: %s' % dirName)
+    for fname in fileList:
+        if fname.endswith('.py'):
+            if dirName=='.':
+                print('%s' % fname)
+            else:
+                print('%s' % (dirName+os.sep+fname))
+            a = py_compile.compile(dirName+os.sep+fname,cfile=dirName+os.sep+fname+'c')
+            os.remove(dirName+os.sep+fname)
+        
