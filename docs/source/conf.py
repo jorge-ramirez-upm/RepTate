3fd1503853e3911d9833c306a71d7259f3fffcaa
Jorge Ramirez <jorge.ramirez@upm.es>
2020-05-17 21:26:34 +0200

    Better class diagrams. Some links in the diagrams work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,6 +45,9 @@ extensions = [
     'sphinx.ext.graphviz'
 ]
 
+# -- Options for graphviz extension ---------------------------------------
+graphviz_output_format = 'svg'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
