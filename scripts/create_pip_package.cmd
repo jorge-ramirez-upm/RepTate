7231ed4b68d293c9ea16a60420bc94fdee097397
Victor Boudara <victor.boudara@gmail.com>
2020-05-25 21:01:35 +0200

    add comments to pip package script

--- scripts/create_pip_package.cmd
+++ scripts/create_pip_package.cmd
@@ -1,3 +1,14 @@
+REM source: https://dzone.com/articles/executable-package-pip-install
+REM Create pypirc: The Pypirc file stores the PyPi repository information. Create a file in the home directory
+REM for Windows :  C:\Users\UserName\.pypirc 
+REM for *nix :   ~/.pypirc 
+REM And add the following content to it. Replace USERNAME with your username.
+REM   [distutils] 
+REM   index-servers=pypi
+REM   [pypi] 
+REM   repository = https://upload.pypi.org/legacy/ 
+REM   username = USERNAME
+
 REM install needed packages first
 python -m pip install --upgrade pip setuptools wheel
 python -m pip install tqdm
