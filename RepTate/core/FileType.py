b09d8de6a01ea018baaa1fec542d268d4ae391cc
Jorge Ramirez <jorge.ramirez@upm.es>
2026-05-12 12:22:31 +0200

    Small bug in FileType.py

--- RepTate/core/FileType.py
+++ RepTate/core/FileType.py
@@ -231,7 +231,7 @@ class TXTColumnFile(object):
     ) -> File | None:
         """Gets all the data from the file"""
         if not os.path.isfile(filename):
-            print('File "%s" does not exists' % f)  # pyright: ignore[reportUnboundVariable]
+            print('File "%s" does not exists' % filename)  # pyright: ignore[reportUnboundVariable]
             return
         file = File(filename, self, parent_dataset, axarr)
         f = open(filename, "r", encoding="latin-1")
@@ -324,7 +324,7 @@ class ExcelFile(object):
     ) -> File | None:
         """Read Excel File"""
         if not os.path.isfile(filename):
-            print('File "%s" does not exists' % f)  # pyright: ignore[reportUndefinedVariable]
+            print('File "%s" does not exists' % filename)  # pyright: ignore[reportUndefinedVariable]
             return
         file = File(filename, self, parent_dataset, axarr)
         wb = load_workbook(filename)
