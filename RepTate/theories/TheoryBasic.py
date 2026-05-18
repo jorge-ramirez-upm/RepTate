Corrected units React bug. Use QTheory parameter helpers in remaining scalar theory reads

RepTate/theories/TheoryBasic.py
@@ -145,7 +145,7 @@ class TheoryPolynomial(QTheory):
             nold = self.parameter_int("n")
             Aold = np.zeros(nold + 1)
             for i in range(nold + 1):
-                Aold[i] = self.parameters["A%d" % i].value
+                Aold[i] = self.parameter_float("A%d" % i)
                 del self.parameters["A%d" % i]
 
             nnew: Any = value
@@ -181,7 +181,7 @@ class TheoryPolynomial(QTheory):
         tt.data[:, 0] = ft.data[:, 0]
         n = self.parameter_int("n")
         for i in range(n + 1):
-            a = self.parameters["A%d" % i].value
+            a = self.parameter_float("A%d" % i)
             for j in range(1, tt.num_columns):
                 tt.data[:, j] += a * tt.data[:, 0] ** i
 
@@ -478,7 +478,7 @@ class TheoryAlgebraicExpression(QTheory):
             nold = self.parameter_int("n")
             Aold = np.zeros(nold)
             for i in range(nold):
-                Aold[i] = self.parameters["A%d" % i].value
+                Aold[i] = self.parameter_float("A%d" % i)
                 del self.parameters["A%d" % i]
 
             nnew: Any = value
@@ -512,7 +512,7 @@ class TheoryAlgebraicExpression(QTheory):
         tt.data = np.zeros((tt.num_rows, tt.num_columns))
         tt.data[:, 0] = ft.data[:, 0]
 
-        expression: Any = self.parameters["expression"].value
+        expression = self.parameter_str("expression")
         params = set(re.findall(r"A\d{1,2}", expression))
         nparams = len(params)
         maxparamindex = -1
@@ -538,7 +538,7 @@ class TheoryAlgebraicExpression(QTheory):
 
             self.safe_dict["x"] = tt.data[:, 0]
             for i in range(n):
-                self.safe_dict["A%d" % i] = self.parameters["A%d" % i].value
+                self.safe_dict["A%d" % i] = self.parameter_float("A%d" % i)
 
             try:
                 y = eval(expression, {"__builtins__": None}, self.safe_dict)

RepTate/theories/TheoryDebyeModes.py
@@ -175,7 +175,7 @@ class TheoryDebyeModesFrequency(QTheory):
         wold = np.logspace(wminold, wmaxold, nmodesold)
         Gold = np.zeros(nmodesold)
         for i in range(nmodesold):
-            Gold[i] = self.parameters["logDe%02d" % i].value
+            Gold[i] = self.parameter_float("logDe%02d" % i)
             del self.parameters["logDe%02d" % i]
 
         nmodesnew = value

RepTate/theories/TheoryLP2RLVE.py
@@ -765,9 +765,9 @@ class TheoryLP2RLVE(QTheory):
 
     def default_lognormal_component(self):
         """Return a default lognormal component from the visible Mw/PDI/n values."""
-        npoly = int(self.parameters["n"].value)
-        mw = float(self.parameters["Mw"].value)
-        pdi = float(self.parameters["PDI"].value)
+        npoly = self.parameter_int("n")
+        mw = self.parameter_float("Mw")
+        pdi = self.parameter_float("PDI")
         return self.make_lognormal_component(
             weight=1.0,
             npoly=npoly,
@@ -1235,31 +1235,31 @@ class TheoryLP2RLVE(QTheory):
     def _build_solver(self):
         """Create and configure a solver instance from the current parameters."""
         material = _lp2r.Material()
-        m_kuhn = float(self.parameters["MK"].value)
-        m_e = float(self.parameters["Me"].value)
+        m_kuhn = self.parameter_float("MK")
+        m_e = self.parameter_float("Me")
         material.m_kuhn = m_kuhn * 1000.0
         material.m_e = m_e * 1000.0
-        material.g0 = self.parameters["G0"].value
-        material.tau_e = self.parameters["tau_e"].value
-        material.g_glass = self.parameters["G_glass"].value
-        material.tau_glass = self.parameters["tau_glass"].value
-        material.beta_glass = self.parameters["beta_glass"].value
+        material.g0 = self.parameter_float("G0")
+        material.tau_e = self.parameter_float("tau_e")
+        material.g_glass = self.parameter_float("G_glass")
+        material.tau_glass = self.parameter_float("tau_glass")
+        material.beta_glass = self.parameter_float("beta_glass")
 
         controls = _lp2r.Controls()
-        controls.alpha = self.parameters["alpha"].value
-        controls.t_cr_start = self.parameters["t_cr_start"].value
-        controls.delta_cr = self.parameters["delta_cr"].value
-        controls.b_zeta = self.parameters["b_zeta"].value
-        controls.a_eq = self.parameters["a_eq"].value
-        controls.b_eq = self.parameters["b_eq"].value
-        controls.ret_pref = self.parameters["ret_pref"].value
-        controls.ret_pref_0 = self.parameters["ret_pref_0"].value
-        controls.ret_switch_exponent = self.parameters["ret_switch_exponent"].value
-        controls.rept_switch_factor = self.parameters["rept_switch_factor"].value
-        controls.rouse_switch_factor = self.parameters["rouse_switch_factor"].value
-        controls.disentanglement_switch = self.parameters["disentanglement_switch"].value
-        controls.start_time = self.parameters["start_time"].value
-        controls.time_ratio = self.parameters["time_ratio"].value
+        controls.alpha = self.parameter_float("alpha")
+        controls.t_cr_start = self.parameter_float("t_cr_start")
+        controls.delta_cr = self.parameter_float("delta_cr")
+        controls.b_zeta = self.parameter_float("b_zeta")
+        controls.a_eq = self.parameter_float("a_eq")
+        controls.b_eq = self.parameter_float("b_eq")
+        controls.ret_pref = self.parameter_float("ret_pref")
+        controls.ret_pref_0 = self.parameter_float("ret_pref_0")
+        controls.ret_switch_exponent = self.parameter_float("ret_switch_exponent")
+        controls.rept_switch_factor = self.parameter_float("rept_switch_factor")
+        controls.rouse_switch_factor = self.parameter_float("rouse_switch_factor")
+        controls.disentanglement_switch = self.parameter_float("disentanglement_switch")
+        controls.start_time = self.parameter_float("start_time")
+        controls.time_ratio = self.parameter_float("time_ratio")
 
         solver = _lp2r.Solver(material, controls)
         components = self.validate_lp2r_components(self.current_lp2r_components())
@@ -1319,7 +1319,7 @@ class TheoryLP2RLVE(QTheory):
 
         freq_min = float(np.min(omega_data))
         freq_max = float(np.max(omega_data))
-        freq_ratio = float(self.parameters["freq_ratio"].value)
+        freq_ratio = self.parameter_float("freq_ratio")
         if freq_ratio <= 1.0:
             self.Qprint("<font color=red><b>LP2R freq_ratio must be larger than 1</b></font>")
             self._clear_table(tt)

RepTate/theories/TheoryMaxwellModes.py
@@ -192,7 +192,7 @@ class TheoryMaxwellModesFrequency(QTheory):
             wold = _logspace(wminold, wmaxold, nmodesold)
             Gold = np.zeros(nmodesold)
             for i in range(nmodesold):
-                Gold[i] = self.parameters["logG%02d" % i].value
+                Gold[i] = self.parameter_float("logG%02d" % i)
                 del self.parameters["logG%02d" % i]
 
             nmodesnew = int(value)

RepTate/theories/TheoryReactMix.py
@@ -173,7 +173,7 @@ class TheoryReactMix(QTheory):
                 np.power(10, rch.return_binsandbob_multi_lgmid(c_i)) * 1.0e-3
             )
             tt.data[i - 1, 1] = rch.return_binsandbob_multi_wt(c_i)
-            tt.data[i - 1, 2] = rch.return_binsandbob_multi_avg(c_i) * 1.0e-3
+            tt.data[i - 1, 2] = rch.return_binsandbob_multi_avg(c_i)
             tt.data[i - 1, 3] = rch.return_binsandbob_multi_avbr(c_i)
 
         totpoly = 0

RepTate/theories/TheoryShanbhagMaxwellModes.py
@@ -193,7 +193,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         # GRAPHIC MODES
         self.graphicmodes = []
         self.spectrum = []
-        ns = int(self.parameters["ns"].value)
+        ns = self.parameter_int("ns")
         self.scont = np.zeros(ns)
         self.Hcont = np.zeros(ns)
         self.sdisc = np.zeros(ns)
@@ -658,7 +658,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         #
         rhoF = interp1d_any(lam, rho, bounds_error=False, fill_value=(rho[0], rho[-1]))
 
-        rho_cutoff = float(self.parameters["rho_cutoff"].value)
+        rho_cutoff = self.parameter_float("rho_cutoff")
         if rhoF(lamC) <= rho_cutoff:
             try:
                 eridx = (np.abs(rhoF(lami) - rho_cutoff)).argmin()
@@ -687,13 +687,13 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
                     also gives an error estimate
         """
 
-        plateau = bool(self.parameters["plateau"].value)
+        plateau = self.parameter_bool("plateau")
         if plateau:
             G0 = argv[0]
 
-        lamDensity = int(self.parameters["lamDensity"].value)
-        lam_max = float(self.parameters["lam_max"].value)
-        lam_min = float(self.parameters["lam_min"].value)
+        lamDensity = self.parameter_int("lamDensity")
+        lam_max = self.parameter_float("lam_max")
+        lam_min = self.parameter_float("lam_min")
         npoints = int(lamDensity * (np.log10(lam_max) - np.log10(lam_min)))
         hlam = (lam_max / lam_min) ** (1.0 / (npoints - 1.0))
         lam = lam_min * hlam ** np.arange(npoints)
@@ -762,7 +762,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         #
         # Dialling in the Smoothness Factor
         #
-        SmFacLam = int(self.parameters["SmFacLam"].value)
+        SmFacLam = self.parameter_int("SmFacLam")
         if SmFacLam > 0:
             lamM = np.exp(np.log(lamM) + SmFacLam * (max(np.log(lam)) - np.log(lamM)))
         elif SmFacLam < 0:
@@ -1089,14 +1089,14 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         tt.data = np.zeros((tt.num_rows, tt.num_columns))
         tt.data[:, 0] = w
 
-        ns = int(self.parameters["ns"].value)
+        ns = self.parameter_int("ns")
         wmin = w[0]
         wmax = w[n - 1]
         smin: Any
         smax: Any
 
         # determine frequency window
-        FreqEnd = int(self.parameters["FreqEnd"].value)
+        FreqEnd = self.parameter_int("FreqEnd")
         if FreqEnd == 1:
             smin = np.exp(-np.pi / 2) / wmax
             smax = np.exp(np.pi / 2) / wmin
@@ -1114,7 +1114,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
 
         self.Qprint("Initial Set up...", end="")
         tic = time.time()
-        plateau = bool(self.parameters["plateau"].value)
+        plateau = self.parameter_bool("plateau")
         if plateau:
             Hgs, G0 = self.InitializeH(Gexp, s, kernMat, np.min(Gexp))
         else:
@@ -1126,7 +1126,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
 
         # Find Optimum Lambda with 'lcurve'
         self.Qprint("Building the L-curve ...", end="")
-        lamC = float(self.parameters["lamC"].value)
+        lamC = self.parameter_float("lamC")
         if lamC == 0:
             if plateau:
                 lamC, lam, rho, eta, logP, Hlam = self.lcurve(Gexp, Hgs, kernMat, G0)
@@ -1150,7 +1150,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         self.Qprint("done ({0:.1f} s)".format(te))
 
         # Save inferred H(s) and Gw
-        if float(self.parameters["lamC"].value) != 0:
+        if self.parameter_float("lamC") != 0:
             if plateau:
                 self.K = self.kernel_prestore(H, kernMat, G0)
                 # np.savetxt('output/H.dat', np.c_[s, H], fmt='%e', header='G0 = {0:0.3e}'.format(G0))
@@ -1194,7 +1194,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         self.Qprint("<b>DISCRETE SPECTRUM</b>")
 
         # range of N scanned
-        MaxNumModes = int(self.parameters["MaxNumModes"].value)
+        MaxNumModes = self.parameter_int("MaxNumModes")
         if MaxNumModes == 0:
             Nmax = min(np.floor(3.0 * np.log10(max(w) / min(w))), n / 4)
             # maximum N
@@ -1208,7 +1208,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         npts = len(Nv)
 
         # range of wtBaseDist scanned
-        deltaBaseWeightDist = float(self.parameters["deltaBaseWeightDist"].value)
+        deltaBaseWeightDist = self.parameter_float("deltaBaseWeightDist")
         wtBase = deltaBaseWeightDist * np.arange(1, int(1.0 / deltaBaseWeightDist))
         AICbst = np.zeros(len(wtBase))
         Nbst = np.zeros(len(wtBase))  # nominal number of modes
@@ -1263,7 +1263,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         else:
             g = g[indx]
 
-        minTauSpacing = float(self.parameters["minTauSpacing"].value)
+        minTauSpacing = self.parameter_float("minTauSpacing")
         while min(tauSpacing) < minTauSpacing and itry < 3:
             self.Qprint("Tau Spacing < minTauSpacing")
 
@@ -1377,7 +1377,7 @@ class TheoryShanbhagMaxwellModesFrequency(QTheory):
         view = self.parent_dataset.parent_application.current_view
         data_table_tmp = DataTable(self.axarr)
         data_table_tmp.num_columns = 3
-        ns = int(self.parameters["ns"].value)
+        ns = self.parameter_int("ns")
         data_table_tmp.num_rows = ns
         data_table_tmp.data = np.zeros((ns, 3))
         data_table_tmp.data[:, 0] = np.reciprocal(self.scont)
@@ -1531,7 +1531,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         # GRAPHIC MODES
         self.graphicmodes = []
         self.spectrum = []
-        ns = int(self.parameters["ns"].value)
+        ns = self.parameter_int("ns")
         self.scont = np.zeros(ns)
         self.Hcont = np.zeros(ns)
         self.sdisc = np.zeros(ns)
@@ -1871,7 +1871,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         #
         rhoF = interp1d_any(lam, rho)
 
-        rho_cutoff = float(self.parameters["rho_cutoff"].value)
+        rho_cutoff = self.parameter_float("rho_cutoff")
         if rhoF(lamC) <= rho_cutoff:
             try:
                 eridx = (np.abs(rhoF(lami) - rho_cutoff)).argmin()
@@ -1900,13 +1900,13 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
                     also gives an error estimate
 
         """
-        plateau = bool(self.parameters["plateau"].value)
+        plateau = self.parameter_bool("plateau")
         if plateau:
             G0 = argv[0]
 
-        lamDensity = int(self.parameters["lamDensity"].value)
-        lam_max = float(self.parameters["lam_max"].value)
-        lam_min = float(self.parameters["lam_min"].value)
+        lamDensity = self.parameter_int("lamDensity")
+        lam_max = self.parameter_float("lam_max")
+        lam_min = self.parameter_float("lam_min")
         npoints = int(lamDensity * (np.log10(lam_max) - np.log10(lam_min)))
         hlam = (lam_max / lam_min) ** (1.0 / (npoints - 1.0))
         lam = lam_min * hlam ** np.arange(npoints)
@@ -1975,7 +1975,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         #
         # Dialling in the Smoothness Factor
         #
-        SmFacLam = int(self.parameters["SmFacLam"].value)
+        SmFacLam = self.parameter_int("SmFacLam")
         if SmFacLam > 0:
             lamM = np.exp(np.log(lamM) + SmFacLam * (max(np.log(lam)) - np.log(lamM)))
         elif SmFacLam < 0:
@@ -2391,14 +2391,14 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         self.tfit = np.copy(t)
 
         n = len(t)
-        ns = int(self.parameters["ns"].value)
+        ns = self.parameter_int("ns")
         tmin = t[0]
         tmax = t[n - 1]
         smin: Any
         smax: Any
 
         # determine frequency window
-        FreqEnd = int(self.parameters["FreqEnd"].value)
+        FreqEnd = self.parameter_int("FreqEnd")
         if FreqEnd == 1:
             smin = np.exp(-np.pi / 2) * tmin
             smax = np.exp(np.pi / 2) * tmax
@@ -2414,7 +2414,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
 
         kernMat = self.getKernMat(s, t)
         tic = time.time()
-        plateau = bool(self.parameters["plateau"].value)
+        plateau = self.parameter_bool("plateau")
 
         self.Qprint("Initial Set up...", end="")
         if plateau:
@@ -2427,7 +2427,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         tic = time.time()
 
         # Find Optimum Lambda with 'lcurve'
-        lamC = float(self.parameters["lamC"].value)
+        lamC = self.parameter_float("lamC")
         self.Qprint("Building the L-curve ...", end="")
         if lamC == 0:
             if plateau:
@@ -2475,7 +2475,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         self.Qprint("<b>DISCRETE SPECTRUM</b>")
 
         # range of N scanned
-        MaxNumModes = int(self.parameters["MaxNumModes"].value)
+        MaxNumModes = self.parameter_int("MaxNumModes")
 
         if MaxNumModes == 0:
             Nmax = min(np.floor(3.0 * np.log10(max(t) / min(t))), n / 4)
@@ -2490,7 +2490,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         npts = len(Nv)
 
         # range of wtBaseDist scanned
-        deltaBaseWeightDist = float(self.parameters["deltaBaseWeightDist"].value)
+        deltaBaseWeightDist = self.parameter_float("deltaBaseWeightDist")
         wtBase = deltaBaseWeightDist * np.arange(1, int(1.0 / deltaBaseWeightDist))
         AICbst = np.zeros(len(wtBase))
         Nbst = np.zeros(len(wtBase))  # nominal number of modes
@@ -2549,7 +2549,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         else:
             g = g[indx]
 
-        minTauSpacing = float(self.parameters["minTauSpacing"].value)
+        minTauSpacing = self.parameter_float("minTauSpacing")
         while min(tauSpacing) < minTauSpacing and itry < 3:
             self.Qprint("Tau Spacing < minTauSpacing")
 
@@ -2673,7 +2673,7 @@ class TheoryShanbhagMaxwellModesTime(QTheory):
         view = self.parent_dataset.parent_application.current_view
         data_table_tmp = DataTable(self.axarr)
         data_table_tmp.num_columns = 2
-        ns = int(self.parameters["ns"].value)
+        ns = self.parameter_int("ns")
         data_table_tmp.num_rows = ns
         data_table_tmp.data = np.zeros((ns, 2))
         data_table_tmp.data[:, 0] = self.scont

RepTate/theories/TheoryTobitaCSTR.py
@@ -215,7 +215,7 @@ reactor during free-radical polymerisation.
         lambda_ = self.parameter_float("lambda")
         sigma = self.parameter_float("sigma")
         numtomake = int(round(self.parameter_float("num_to_make")))
-        monmass: Any = self.parameters["mon_mass"].display_value()
+        monmass = self.parameter_float("mon_mass")*1000 # Internally stored as kg/mol. Theory expects g/mol
         Me = self.parameter_float("Me")
         nbins = int(round(self.parameter_float("nbin")))
         rch.set_do_prio_senio(ct.c_bool(self.do_priority_seniority))
@@ -365,7 +365,7 @@ reactor during free-radical polymerisation.
                     np.power(10, rch.react_dist[ndist].contents.lgmid[i]) * 1.0e-3
                 )
                 tt.data[i - 1, 1] = rch.react_dist[ndist].contents.wt[i]
-                tt.data[i - 1, 2] = rch.react_dist[ndist].contents.avg[i] * 1.0e-3
+                tt.data[i - 1, 2] = rch.react_dist[ndist].contents.avg[i]
                 tt.data[i - 1, 3] = rch.react_dist[ndist].contents.avbr[i]
             rch.end_print(self, ndist, self.do_priority_seniority)
             rch.prio_and_senio(self, f, ndist, self.do_priority_seniority)