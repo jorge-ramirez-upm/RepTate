e574ca2e18729c8891cc8e285c63d3b8443635e0
Jorge Ramirez <jorge.ramirez@upm.es>
2026-09-09 13:38:07 +0200

    Small compatibility error with Numpy versions

--- RepTate/core/FileType.py
+++ RepTate/core/FileType.py
@@ -321,7 +321,7 @@ class TXTColumnFile(object):
                         rawdata.append(float("nan"))
         file.data_table.num_rows = int(len(rawdata) / file.data_table.num_columns)
         file.data_table.data = np.reshape(
-            rawdata, newshape=(file.data_table.num_rows, file.data_table.num_columns)
+            rawdata, (file.data_table.num_rows, file.data_table.num_columns)
         )
         file.data_table.data = file.data_table.data[
             file.data_table.data[:, 0].argsort()
